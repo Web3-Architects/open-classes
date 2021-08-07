cb404b5e1472ab339da76f6f95e6819f8d5f2868
Iustin Nita <iustin.nitza@gmail.com>
2021-08-07 18:45:03 +0200

    feat(): add tests and dependencies wip

--- contracts/Lesson.sol
+++ contracts/Lesson.sol
@@ -24,12 +24,18 @@ contract Lesson is VRFConsumerBase {
     mapping(address => uint256) private s_results;
 
     /**
-     * Constructor inherits VRFConsumerBase
+     * @notice Constructor inherits VRFConsumerBase
      *
-     * Network: Kovan
-     * Chainlink VRF Coordinator address: 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9
-     * LINK token address:                0xa36085F69e2889c224210F603D836748e7dC0088
-     * Key Hash: 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
+     * @dev NETWORK: KOVAN
+     * @dev   Chainlink VRF Coordinator address: 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9
+     * @dev   LINK token address:                0xa36085F69e2889c224210F603D836748e7dC0088
+     * @dev   Key Hash:   0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
+     * @dev   Fee:        0.1 LINK (100000000000000000)
+     *
+     * @param vrfCoordinator address of the VRF Coordinator
+     * @param link address of the LINK token
+     * @param keyHash bytes32 representing the hash of the VRF job
+     * @param fee uint256 fee to pay the VRF oracle
      */
 
     constructor(
@@ -47,6 +53,10 @@ contract Lesson is VRFConsumerBase {
         s_keyHash = keyHash;
         s_fee = fee;
     }
+
+    // function hashSeriesNumber(string calldata series, uint256 number) external pure returns (bytes32) {
+    //     return keccak256(abi.encode(number, series));
+    // }
 
     function requestRandom(address user) public returns (bytes32 requestId) {
         require(LINK.balanceOf(address(this)) >= s_fee, "Not enough LINK to pay fee");
