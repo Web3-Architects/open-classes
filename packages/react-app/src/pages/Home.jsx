7f9875aa88497ed044d76b974a3165a2fb682ff4
Iustin Nita <iustin.nitza@gmail.com>
2021-08-08 18:46:31 +0200

    improve pages, create cool section

--- packages/react-app/src/pages/Home.jsx
+++ packages/react-app/src/pages/Home.jsx
@@ -35,6 +35,7 @@ import {
   footerNavigation,
   navigation,
 } from "../mocks/home";
+import "./Home.css";
 
 export default function Home() {
   return (
@@ -150,99 +151,6 @@ export default function Home() {
             </div>
           </div>
 
-          {/* Feature section with grid */}
-          <div className="relative bg-white py-16 sm:py-24 lg:py-32">
-            <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
-              <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">
-                Deploy faster
-              </h2>
-              <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
-                Everything you need to deploy your app
-              </p>
-              <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
-                Phasellus lorem quam molestie id quisque diam aenean nulla in.
-                Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend
-                condimentum id viverra nulla.
-              </p>
-              <div className="mt-12">
-                <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-                  {features.map((feature) => (
-                    <div key={feature.name} className="pt-6">
-                      <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
-                        <div className="-mt-6">
-                          <div>
-                            <span className="inline-flex items-center justify-center p-3 bg-gradient-to-r from-teal-500 to-cyan-600 rounded-md shadow-lg">
-                              <feature.icon
-                                className="h-6 w-6 text-white"
-                                aria-hidden="true"
-                              />
-                            </span>
-                          </div>
-                          <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">
-                            {feature.name}
-                          </h3>
-                          <p className="mt-5 text-base text-gray-500">
-                            {feature.description}
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* Testimonial section */}
-          <div className="pb-16 bg-gradient-to-r from-teal-500 to-cyan-600 lg:pb-0 lg:z-10 lg:relative">
-            <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-8">
-              <div className="relative lg:-my-8">
-                <div
-                  aria-hidden="true"
-                  className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden"
-                />
-                <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
-                  <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
-                    <img
-                      className="object-cover lg:h-full lg:w-full"
-                      src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80"
-                      alt=""
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="mt-12 lg:m-0 lg:col-span-2 lg:pl-8">
-                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:px-0 lg:py-20 lg:max-w-none">
-                  <blockquote>
-                    <div>
-                      <svg
-                        className="h-12 w-12 text-white opacity-25"
-                        fill="currentColor"
-                        viewBox="0 0 32 32"
-                        aria-hidden="true"
-                      >
-                        <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
-                      </svg>
-                      <p className="mt-6 text-2xl font-medium text-white">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Sed urna nulla vitae laoreet augue. Amet feugiat est
-                        integer dolor auctor adipiscing nunc urna, sit.
-                      </p>
-                    </div>
-                    <footer className="mt-6">
-                      <p className="text-base font-medium text-white">
-                        Judith Black
-                      </p>
-                      <p className="text-base font-medium text-cyan-100">
-                        CEO at PureInsights
-                      </p>
-                    </footer>
-                  </blockquote>
-                </div>
-              </div>
-            </div>
-          </div>
-
           {/* Blog section */}
           <div className="relative bg-gray-50 py-16 sm:py-24 lg:py-32">
             <div className="relative">
@@ -318,6 +226,46 @@ export default function Home() {
                         </div>
                       </div>
                     </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+
+          {/* Blog section */}
+          <div className="relative bg-gray-50 py-16 sm:py-24 lg:py-32 learn-section">
+            <div className="relative">
+              <div className="text-center mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+                <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">
+                  Learn
+                </h2>
+                <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+                  Helpful Resources
+                </p>
+                <p className="mt-5 mx-auto max-w-prose text-xl text-gray-500">
+                  Phasellus lorem quam molestie id quisque diam aenean nulla in.
+                  Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend
+                  condimentum id viverra nulla.
+                </p>
+              </div>
+              <div className="mt-12 mx-auto max-w-md px-4 grid gap-8 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl blog-section">
+                {blogPosts.map((post) => (
+                  <div class="blog-item gamut-w6lb1f-Column e1y0e4q30">
+                    <a
+                      href="#"
+                      class="blog-link noUnderline__3auQH9YTaYeT71hkb5qMQq"
+                    >
+                      <div class="blog-box-outer gamut-1ekicai-Box-Card eeujxae0">
+                        <div class="blog-box-inner gamut-1vofu7t-Box ebnwbv90">
+                          <p class="first-link gamut-1f97w4s-Text e8i0p5k0">
+                            Explore All
+                          </p>
+                          <p class="second-link gamut-c3dt2s-Text e8i0p5k0">
+                            Javascript
+                          </p>
+                        </div>
+                      </div>
+                    </a>
                   </div>
                 ))}
               </div>
