f0f937730860296fb377766934ba05a081e23fbf
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-08-14 18:14:47 +0200

    add validate challenge section in Lesson page

---
+++ packages/react-app/src/components/ValidateChallenge.jsx
@@ -0,0 +1,57 @@
+
+import { useState, useEffect, useCallback } from "react";
+import { ethers } from "ethers";
+import {  abis } from "@project/contracts";
+
+import useWeb3Modal from "../hooks/useWeb3Modal";
+
+const CHALLENGE_CONTRACT_ADDRESS = "0x34BaB47Ce1ce166C20cC19a3Db98dc16518E13da";
+
+const ValidateChallenge = () => {
+  const { provider } = useWeb3Modal();
+  const [challengeContract, setChallengeContract] = useState(null);
+
+  useEffect(() => {
+    const contract = new ethers.Contract(CHALLENGE_CONTRACT_ADDRESS, abis.randomNumberConsumer, provider);
+    setChallengeContract(contract)
+  }, [provider])
+
+  const callToValidateChallenge = useCallback(
+    async ({ randomNumber }) => {
+      await challengeContract.validateChallenge();
+    },
+    [challengeContract]);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    let randomNumber = e.target.elements.randomNumber?.value;
+
+    callToValidateChallenge({ randomNumber });
+  };
+  return (
+    <div className="h-24 flex flex-col items-center justify-center">
+        <form onSubmit={handleFormSubmit} className="flex flex-col items-center justify-center">
+        <div
+            className={`w-96 mb-4 border transition duration-150 ease-in-out focus-within:border-primary border-gray-500`}
+      >
+            <label
+                htmlFor="validate"
+                className={`text-xs text-primary font-light placeholder-gray-gray4 px-2 pt-1.5`}
+                >
+                Random Number
+            </label>
+            <input
+                type="text"
+                className={`w-full px-2 pb-1.5 text-primary outline-none text-base font-light rounded-md`}
+                id="randomNumber"
+                placeholder={"28933480438792750..."}
+            />
+          </div>
+          <button type="button" className="shadow bg-yellow hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-black-300 font-bold py-2 px-4 mx-auto rounded">SUBMIT</button>
+        </form>
+    </div>
+  )
+};
+
+export default ValidateChallenge;
