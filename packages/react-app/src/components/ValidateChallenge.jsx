98998a2aa4d03801cc54e700a2cdf2e3f51e84ac
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-11-15 22:33:10 +0000

    fix error abi.map error

--- packages/react-app/src/components/ValidateChallenge.jsx
+++ packages/react-app/src/components/ValidateChallenge.jsx
@@ -1,7 +1,6 @@
-
 import { useState, useEffect, useCallback } from "react";
 import { ethers } from "ethers";
-import {  abis } from "@project/contracts";
+import { abis } from "@project/contracts";
 
 import useWeb3Modal from "../hooks/useWeb3Modal";
 
@@ -12,17 +11,22 @@ const ValidateChallenge = () => {
   const [challengeContract, setChallengeContract] = useState(null);
 
   useEffect(() => {
-    const contract = new ethers.Contract(CHALLENGE_CONTRACT_ADDRESS, abis.randomNumberConsumer, provider);
-    setChallengeContract(contract)
-  }, [provider])
+    const contract = new ethers.Contract(
+      CHALLENGE_CONTRACT_ADDRESS,
+      [abis.randomNumberConsumer],
+      provider
+    );
+    setChallengeContract(contract);
+  }, [provider]);
 
   const callToValidateChallenge = useCallback(
     async ({ randomNumber }) => {
-      // TODO: pass DID subject generated from 3ID Connect 
+      // TODO: pass DID subject generated from 3ID Connect
       const DIDSubject = "did:subject";
       await challengeContract.validateChallenge(randomNumber, DIDSubject);
     },
-    [challengeContract]);
+    [challengeContract]
+  );
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -33,27 +37,35 @@ const ValidateChallenge = () => {
   };
   return (
     <div className="h-24 flex flex-col items-center justify-center">
-        <form onSubmit={handleFormSubmit} className="flex flex-col items-center justify-center">
-        <div
-            className={`w-96 mb-4 border transition duration-150 ease-in-out focus-within:border-primary border-gray-500`}
+      <form
+        onSubmit={handleFormSubmit}
+        className="flex flex-col items-center justify-center"
       >
-            <label
-                htmlFor="validate"
-                className={`text-xs text-primary font-light placeholder-gray-gray4 px-2 pt-1.5`}
-                >
-                Random Number
-            </label>
-            <input
-                type="text"
-                className={`w-full px-2 pb-1.5 text-primary outline-none text-base font-light rounded-md`}
-                id="randomNumber"
-                placeholder={"28933480438792750..."}
-            />
-          </div>
-          <button type="button" className="shadow bg-yellow hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-black-300 font-bold py-2 px-4 mx-auto rounded">SUBMIT</button>
-        </form>
+        <div
+          className={`w-96 mb-4 border transition duration-150 ease-in-out focus-within:border-primary border-gray-500`}
+        >
+          <label
+            htmlFor="validate"
+            className={`text-xs text-primary font-light placeholder-gray-gray4 px-2 pt-1.5`}
+          >
+            Random Number
+          </label>
+          <input
+            type="text"
+            className={`w-full px-2 pb-1.5 text-primary outline-none text-base font-light rounded-md`}
+            id="randomNumber"
+            placeholder={"28933480438792750..."}
+          />
+        </div>
+        <button
+          type="button"
+          className="shadow bg-yellow hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-black-300 font-bold py-2 px-4 mx-auto rounded"
+        >
+          SUBMIT
+        </button>
+      </form>
     </div>
-  )
+  );
 };
 
 export default ValidateChallenge;
