e4c497b4f5c62ebf3414fb077ea69f87384ccc1f
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-12-30 16:54:57 +0100

    add emit event button

--- packages/react-app/src/components/ValidateChallenge.jsx
+++ packages/react-app/src/components/ValidateChallenge.jsx
@@ -7,17 +7,18 @@ import useWeb3Modal from "../hooks/useWeb3Modal";
 const CHALLENGE_CONTRACT_ADDRESS = "0x34BaB47Ce1ce166C20cC19a3Db98dc16518E13da";
 
 const ValidateChallenge = () => {
-  const { provider } = useWeb3Modal();
+  const { address, signer } = useWeb3Modal();
   const [challengeContract, setChallengeContract] = useState(null);
 
   useEffect(() => {
+    if (!signer) return;
     const contract = new ethers.Contract(
       CHALLENGE_CONTRACT_ADDRESS,
       abis.randomNumberConsumer.abi,
-      provider
+      signer
     );
     setChallengeContract(contract);
-  }, [provider]);
+  }, [signer]);
 
   const callToValidateChallenge = useCallback(
     async ({ randomNumber }) => {
@@ -35,8 +36,23 @@ const ValidateChallenge = () => {
 
     callToValidateChallenge({ randomNumber });
   };
+
+  const callToEmitEvent = useCallback(async () => {
+    if (!signer || !challengeContract || !address) {
+      alert("You must be connected with your wallet first");
+    }
+    await challengeContract.requestRandom(address);
+  }, [signer, challengeContract, address]);
+
   return (
-    <div className="h-24 flex flex-col items-center justify-center">
+    <div className="h-28 my-6 flex flex-col items-center justify-center">
+      <button
+        type="button"
+        className="shadow bg-yellow hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-black-300 font-bold mt-2 mb-8 py-2 px-4 mx-auto rounded"
+        onClick={callToEmitEvent}
+      >
+        EMIT EVENT
+      </button>
       <form
         onSubmit={handleFormSubmit}
         className="flex flex-col items-center justify-center"
